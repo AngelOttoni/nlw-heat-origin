23093f54c6f78c47d0aa773c250e0c1c4fbdcf18
AngelOttoni <angel.m.ottoni@gmail.com>
2021-10-20 21:19:25 -0300

    script criado para troca de usuários dos links

---
+++ myscript.js
@@ -0,0 +1,24 @@
+const LinksSocialMedia = {
+  github: 'jakeliny',
+  youtube: 'jakelinygracielly',
+  facebook: 'maykbrito',
+  instagram: 'jakeliny.gracielly',
+  twitter: 'jakelinytec'
+  // github: 'AngelOttoni',
+  // youtube: '',
+  // instagram: 'angelinaottoni',
+  // facebook: 'AngelMOttoni',
+  // twitter: "MMeiras"
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+
+    alert(li.children[0].href)
+  }
+}
+
+changeSocialMediaLinks();
