9b031691c0540e3d6a44834aecb896822aeb761a
AngelOttoni <angel.m.ottoni@gmail.com>
2021-10-21 22:10:47 -0300

    consumo de API GitHub users implementado

--- myscript.js
+++ myscript.js
@@ -1,24 +1,33 @@
-const LinksSocialMedia = {
-  github: 'jakeliny',
-  youtube: 'jakelinygracielly',
-  facebook: 'maykbrito',
-  instagram: 'jakeliny.gracielly',
-  twitter: 'jakelinytec'
-  // github: 'AngelOttoni',
-  // youtube: '',
-  // instagram: 'angelinaottoni',
-  // facebook: 'AngelMOttoni',
-  // twitter: "MMeiras"
+const UsersSocialMedia = {
+  github: 'AngelOttoni',
+  youtube: '',
+  instagram: 'angelinaottoni',
+  facebook: 'AngelMOttoni',
+  twitter: 'MMeiras'
 }
 
-function changeSocialMediaLinks() {
+function changeSocialMediaUsers() {
   for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
 
-    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
-
-    alert(li.children[0].href)
+    li.children[0].href = `https://${social}.com/${UsersSocialMedia[social]}`
   }
 }
 
-//changeSocialMediaLinks();
+changeSocialMediaUsers()
+
+function getGitHubProfileInfo() {
+  const url = `https://api.github.com/users/${UsersSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      //userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfo()
